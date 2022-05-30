f93181b2625dbc4c5e1a0485f27f8f9d56dc37e6
KLAIIIII <guyyg03546@gmail.com>
2022-05-30 14:52:34 +0700

    update

---
+++ for.js
@@ -0,0 +1,12 @@
+
+for(var i = 1; i<=2;i++){
+    // console.log("2 x " + i + "=",2*i)
+    console.log(`2 x ${i} = ${2*i}`)
+}
+
+console.log("==========")
+
+var num = [1,2,3,4,5,6,7,8,9,10,11,12]
+for(let number of num){
+    console.log(`3 x ${number} = ${3 * number}`)
+}
