b0f13e4d79d6fe33be9a2eaf71ae72e1e2a167aa
KLAIIIII <guyyg03546@gmail.com>
2022-05-30 16:41:36 +0700

    update

---
+++ fun201.js
@@ -0,0 +1,8 @@
+// Declaration Anonymous function
+let message = function(text,name){
+    return `${text} ${name}`
+}
+
+// Call function
+let show = message("Wellcome","Mark Zakerberg")
+console.log(show)
