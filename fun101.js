d55f5f100bcaf9346749f82edfb92137ee725767
KLAIIIII <guyyg03546@gmail.com>
2022-05-30 16:17:36 +0700

    update

---
+++ fun101.js
@@ -0,0 +1,7 @@
+function myFunction(message,age){
+    let fullName = "Mark Zakerberg"
+    console.log(`${message} ${fullName} your age ${age} year old`)
+}
+
+//Call Function
+myFunction("Hello",25)
---
+++ fun102.js
@@ -0,0 +1,8 @@
+//1.Declaration Function
+function mull(x,y,z=500){
+    let c = x*y
+    console.log(`x * y = ${c}`)
+}
+
+//2.call Function
+mull(20,30,50)
